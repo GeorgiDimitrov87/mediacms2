8057eab0a3ef7e981c33a693f0ababd091b077e7
GeorgiDimitrov87 <gdimitrovau@gmail.com>
2022-01-12 11:42:54 +0100

    first

---
+++ frontend/src/templates/config/installation/contents.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  header: {
+    right: '',
+  },
+  sidebar: {
+    navMenuItems: [
+      {
+        text: 'About',
+        link: './about.html',
+        icon: 'contact_support',
+      },
+      {
+        text: 'Terms',
+        link: './terms.html',
+        icon: 'description',
+      },
+      {
+        text: 'Contact',
+        link: './contact.html',
+        icon: 'alternate_email',
+      },
+    ],
+    belowNavMenu: null,
+    footer: 'Powered by <a href="biites.com" title="Biites" target="_blank">Biites</a>',
+  },
+  uploader: {
+    belowUploadArea: '',
+    postUploadMessage: '',
+  },
+  notifications: {
+    messages: {
+      addToLiked: 'Added to liked media',
+      removeFromLiked: 'Removed from liked media',
+      addToDisliked: 'Added to disliked media',
+      removeFromDisliked: 'Removed from disliked media',
+    },
+  },
+};
